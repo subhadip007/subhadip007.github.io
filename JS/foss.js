866583364bf17c6bd88d6e098e3b72b89db66be1
subhadip <deathpeace007@gamil.com>
2020-08-05 07:03:28 +0530

    animation1 added

--- JS/foss.js
+++ JS/foss.js
@@ -22,11 +22,23 @@ $(document).ready(function(){
 // }
 
 
+
+
+
+
 const splash= document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded',(e)=>
 setTimeout(()=>{
   splash.classList.add('display-none');
 },2000)
 )
+
+
+gsap.from('.c1,.c3',{scrollTrigger:{trigger:'.c1,.c3',toggleActions: 'restart pause restart pause'},opacity:0,x:-300,duration: 2,});
+gsap.from('.c2,.c4',{scrollTrigger:{trigger:'.c2,.c4',toggleActions: 'restart pause restart pause'},opacity:0,x:+300,duration: 2,});
+
+
+gsap.from('.fabout',{scrollTrigger:'.about-cont',opacity:0,x: -250, duration: 2,marker: true})
+
 
 

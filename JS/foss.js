288dfdc56c2be632d3b4865b46d4d53bff062727
subhadip <deathpeace007@gamil.com>
2020-08-11 00:11:52 +0530

    animations added

--- JS/foss.js
+++ JS/foss.js
@@ -34,11 +34,21 @@ setTimeout(()=>{
 )
 
 
-gsap.from('.c1,.c3',{scrollTrigger:{trigger:'.c1,.c3',toggleActions: 'restart pause restart pause'},opacity:0,x:-300,duration: 2,});
-gsap.from('.c2,.c4',{scrollTrigger:{trigger:'.c2,.c4',toggleActions: 'restart pause restart pause'},opacity:0,x:+300,duration: 2,});
+gsap.from('.c1,.c3',{scrollTrigger:{trigger:'.c1,.c3',toggleActions: 'restart pause restart pause'},opacity:0,ease:"elastic.out(1, 0.3)",x:-300,duration: 2,});
+gsap.from('.c2,.c4',{scrollTrigger:{trigger:'.c2,.c4',toggleActions: 'restart pause restart pause'},opacity:0,ease:"elastic.out(1, 0.3)",x:+300,duration: 2,});
 
 
-gsap.from('.fabout',{scrollTrigger:'.about-cont',opacity:0,x: -250, duration: 2,marker: true})
+gsap.from('.fabout',{scrollTrigger:'.about-cont',opacity:0,ease: "back.out(1.7)",x: -250, duration: 2,marker: true})
+
+
+let boxT = gsap.timeline()
+
+boxT.to('.box',{duration:2,width:'27vw',delay:0.5,ease:"power4.inOut"})
+.from('.openC',{duration:1,y:'7vw',ease:"power3.out"})
+.to('.box',{opacity:0})
 
 
 
+  
+  
+  

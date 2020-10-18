a7525c3a5e8b35620ca9c1f7953397d07a2ca83b
subhadip <deathpeace007@gamil.com>
2020-10-18 06:08:39 +0530

    mobile version 1.0

--- port.js
+++ port.js
@@ -3,29 +3,7 @@ $(document).ready(function(){
     $('.parallax').parallax();
   });
 
-function adjustWindow(){
-winH=$window.height();
-winW=$window.width();
 
 
 
-if(winH<=550){
-  winH=500
-}
 
-if(winW>=768){
-  var s=skrolle.init({froceHight: false})
-  $slide.hight(winH)
-  s.refresh($('.homeSlide'))
-}else{
-
- var s=skrollr.init()
- s.destroy()
-
-}
-
-if(Modernizr.touch){
-var s=skrollr.init()
-s.destroy();
-}
-}
